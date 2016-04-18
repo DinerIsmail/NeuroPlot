3ae39c2a204a268b26490d09f4afdbde4fd9c5dd
Diner Ismail <ismail.diner@gmail.com>
2016-04-18 12:28:03 +0100

    Added variable for Heroku dynamic port binding

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ var express = require('express'),
     server = require('http').createServer(app),
     io = require('socket.io').listen(server);
 
-server.listen(3000);
+server.listen(process.env.PORT || 3000);
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/public/index.html');
